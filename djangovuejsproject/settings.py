f96861458658a5eeb3001a2ce20e7771370ec36f
Techiediaries <22133314+techiediaries@users.noreply.github.com>
2018-03-31 00:15:26 +0100

    Update settings.py

--- djangovuejsproject/settings.py
+++ djangovuejsproject/settings.py
@@ -155,7 +155,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         #'rest_framework.authentication.BasicAuthentication',
         #'rest_framework.authentication.SessionAuthentication',
-        'rest_framework_jwt.authentication.JSONWebTokenAuthentication'
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     ),
 }
 
